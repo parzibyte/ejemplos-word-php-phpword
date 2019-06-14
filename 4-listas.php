d338d9b41e2115a2f99858efd8fd3ed4286a830a
Luis Cabrera Benito <contacto@parzibyte.me>
2019-06-13 22:39:28 -0500

    Refactorizado y agregado readme

--- 4-listas.php
+++ 4-listas.php
@@ -15,7 +15,6 @@ $propiedades = $documento->getDocInfo();
 $propiedades->setCreator("Luis Cabrera Benito");
 $propiedades->setTitle("Listas");
 
-
 $seccion = $documento->addSection();
 $seccion->addListItem("Elemento con profundidad por defecto");
 $seccion->addListItem("Elemento con profundidad 1", 1);
@@ -32,29 +31,40 @@ $fuente = [
 ];
 $seccion->addListItem("Elemento con profundidad 1 y con fuente", 1, $fuente);
 for ($profundidad = 1; $profundidad < 4; $profundidad++) {
-    $seccion->addListItem("Elemento con profundidad $profundidad, con fuente y tipo de lista TYPE_SQUARE_FILLED", $profundidad, $fuente, array('listType' => ListItem::TYPE_SQUARE_FILLED));
+    $seccion->addListItem("Elemento con profundidad $profundidad, con fuente y tipo de lista TYPE_SQUARE_FILLED", $profundidad, $fuente, [
+        'listType' => ListItem::TYPE_SQUARE_FILLED,
+    ]);
 }
 
 for ($profundidad = 1; $profundidad < 4; $profundidad++) {
-    $seccion->addListItem("Elemento con profundidad $profundidad, con fuente y tipo de lista TYPE_BULLET_FILLED", $profundidad, $fuente, array('listType' => ListItem::TYPE_BULLET_FILLED));
+    $seccion->addListItem("Elemento con profundidad $profundidad, con fuente y tipo de lista TYPE_BULLET_FILLED", $profundidad, $fuente, [
+        'listType' => ListItem::TYPE_BULLET_FILLED,
+    ]);
 }
 
 for ($profundidad = 1; $profundidad < 4; $profundidad++) {
-    $seccion->addListItem("Elemento con profundidad $profundidad, con fuente y tipo de lista TYPE_BULLET_EMPTY", $profundidad, $fuente, array('listType' => ListItem::TYPE_BULLET_EMPTY));
+    $seccion->addListItem("Elemento con profundidad $profundidad, con fuente y tipo de lista TYPE_BULLET_EMPTY", $profundidad, $fuente, [
+        'listType' => ListItem::TYPE_BULLET_EMPTY,
+    ]);
 }
 
 for ($profundidad = 1; $profundidad < 4; $profundidad++) {
-    $seccion->addListItem("Elemento con profundidad $profundidad, con fuente y tipo de lista TYPE_NUMBER", $profundidad, $fuente, array('listType' => ListItem::TYPE_NUMBER));
+    $seccion->addListItem("Elemento con profundidad $profundidad, con fuente y tipo de lista TYPE_NUMBER", $profundidad, $fuente, [
+        'listType' => ListItem::TYPE_NUMBER,
+    ]);
 }
 
 for ($profundidad = 1; $profundidad < 4; $profundidad++) {
-    $seccion->addListItem("Elemento con profundidad $profundidad, con fuente y tipo de lista TYPE_NUMBER_NESTED", $profundidad, $fuente, array('listType' => ListItem::TYPE_NUMBER_NESTED));
+    $seccion->addListItem("Elemento con profundidad $profundidad, con fuente y tipo de lista TYPE_NUMBER_NESTED", $profundidad, $fuente, [
+        'listType' => ListItem::TYPE_NUMBER_NESTED,
+    ]);
 }
 
 for ($profundidad = 1; $profundidad < 4; $profundidad++) {
-    $seccion->addListItem("Elemento con profundidad $profundidad, con fuente y tipo de lista TYPE_ALPHANUM", $profundidad, $fuente, array('listType' => ListItem::TYPE_ALPHANUM));
+    $seccion->addListItem("Elemento con profundidad $profundidad, con fuente y tipo de lista TYPE_ALPHANUM", $profundidad, $fuente, [
+        'listType' => ListItem::TYPE_ALPHANUM,
+    ]);
 }
-
 
 # Para que no diga que se abre en modo de compatibilidad
 $documento->getCompatibility()->setOoxmlVersion(15);
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# ejemplos-word-php-phpword
+# Ejemplos de PHPWord: librería de PHP para trabajar con documentos de Word
+
+#### Publicado por  [**parzibyte**](https://parzibyte.me/blog/author/parzibyte/)  en  junio 13, 2019
+Este post es un índice y recopilación de enlaces para crear documentos de Word con PHP usando la librería de la familia de PHPOffice llamada PHPWord.
+
+He escrito 3 posts sobre esta librería:
+
+1.  [Instalación de PHPWord e interacción con texto, estilos, fuentes y títulos](https://parzibyte.me/blog/2019/06/13/crear-documentos-word-php-phpword/)
+2.  [Listas, tablas e imágenes con PHPWord](https://parzibyte.me/blog/2019/06/13/php-word-agregar-listas-tablas-imagenes-usando-phpword/)
+3.  [Marca de agua, tabla de contenido y gráficas con PHPWord](https://parzibyte.me/blog/2019/06/13/phpword-agregar-marca-agua-toc-graficas-word-php/)
+
+La documentación oficial está  [aquí](https://phpword.readthedocs.io/en/latest/).
+
+El proyecto de la librería en GitHub está  [aquí](https://github.com/PHPOffice/PHPWord/).
+
+Los ejemplos que muestro en los posts, están  [aquí](https://github.com/parzibyte/ejemplos-word-php-phpword).
+
+Algunos otros ejemplos (en inglés) pueden verse  [aquí](https://github.com/PHPOffice/PHPWord/tree/develop/samples).
+
+Recuerda que todos los ejemplos se pueden combinar, además de que el resultado se puede guardar en el disco duro o mandar al navegador para su descarga; o también se pueden enviar por correo, etcétera, pues al final son archivos.
